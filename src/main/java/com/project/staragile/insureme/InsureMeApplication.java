1a6652a825823fe9c4dcb186fc3f455e5d8497e7
Vamsi Krishna Bhagi <bhagivamsi@gmail.com>
2023-07-30 19:53:25 -0400

    Testing Webhook

--- src/main/java/com/project/staragile/insureme/InsureMeApplication.java
+++ src/main/java/com/project/staragile/insureme/InsureMeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsureMeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testing Webhook");
+		System.out.println("Testing Webhook 3");
 		SpringApplication.run(InsureMeApplication.class, args);
 	}
 
